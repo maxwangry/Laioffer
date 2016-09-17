21b687668e30ae4c1d5d5f59b13c8b6f265fba33
dandanbiu <dandanbiu@laioffer.com>
2016-09-16 21:37:50 -0700

    Create repo

---
+++ src/com/laioffer/classone/MyFirstLaiofferClass.java
@@ -0,0 +1,10 @@
+package com.laioffer.classone;
+
+public class MyFirstLaiofferClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello from the other side");
+	}
+
+}
