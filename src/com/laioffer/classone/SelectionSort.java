48be480377a919088fc7acd810ff102e0cd5d4e8
dandanbiu <dandanbiu@laioffer.com>
2016-09-17 22:21:31 -0700

    Complete Selection Sort

---
+++ src/com/laioffer/classone/SelectionSort.java
@@ -0,0 +1,30 @@
+package com.laioffer.classone;
+
+public class SelectionSort {
+
+	public static int[] solve(int[] array) {
+		
+		// Corner cases
+		if (array == null || array.length <= 1) return array;
+		
+		int minIndex = 0, size = array.length;
+		for (int i = 0; i < size - 1; i++) {
+			minIndex = i;
+			// Look for the min
+			for (int j = i + 1; j < size; j++) if (array[j] < array[minIndex]) minIndex = j;
+			// swap
+			int temp = array[i];
+			array[i] = array[minIndex];
+			array[minIndex] = temp;
+		}
+		return array;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = new int[] {7,6,5,4,3,2,1,0};
+		array = solve(array);
+		for (int i = 0; i < array.length; i++) System.out.println(array[i]);
+	}
+
+}
